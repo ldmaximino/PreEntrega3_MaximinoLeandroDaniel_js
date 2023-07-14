f7ed169f13c002e2995560158dd44060efbb4f0d
Leandro Maximino <ldmaximino@gmail.com>
2023-07-13 21:48:06 -0300

    Se agregó fetch a una api para obtener un array de usuarios

---
+++ js/bdUsuarios.js
@@ -0,0 +1,32 @@
+export const bdUsuariosInt = [
+    {
+        email: "juliaAl@gmail.com",
+        clave: "1234",
+        nombre: "Julia"
+    },
+    {
+        email: "daniel-ver@hotmail.com",
+        clave: "1234",
+        nombre: "Daniel"
+    },
+    {
+        email: "raquelpascini@gmail.com",
+        clave: "1234",
+        nombre: "Raquel"
+    },
+    {
+        email: "pepe@outlook.com",
+        clave: "1234",
+        nombre: "José"
+    },
+    {
+        email: "nahim@coderhouse.com",
+        clave: "1234",
+        nombre: "Nahim"
+    },
+    {
+        email: "dinora@hotmail.com",
+        clave: "1234",
+        nombre: "Dinora"
+    }
+];
--- js/clases_funciones.js
+++ js/clases_funciones.js
@@ -1,6 +1,7 @@
 //Importar
 import { bdProductos } from "./bdProductos.js"; //Base de datos de productos disponibles
-import { bdFPago } from "./bdFPago.js"; //opciones de pago
+import { bdFPago } from "./bdFPago.js"; //Base de datos opciones de pago
+import { bdUsuariosInt } from "./bdUsuarios.js";
 
 //------------------------------------------------------------------- DECLARACION DE VARIABLES Y CONSTANTES ----------------------------------------------
 const textoAlternativo = document.getElementById('texto-alternativo');
@@ -14,6 +15,7 @@ const totalesCarrito = document.querySelector(".totales-carrito");
 const modal = document.querySelector(".modal");
 const modalContainer = document.querySelector(".modal-container");
 const modalContainerTitulo = document.querySelector(".modal-container-titulo");
+const modalLogin = document.querySelector(".modal-login");
 const cerrarModal = document.querySelector(".modal-close");
 const vaciarCarrito = document.querySelector("#vaciar-carrito");
 const finalizarCompra = document.querySelector("#finalizar-compra");
@@ -23,11 +25,24 @@ const volverAlCarrito = document.querySelector(".volver-carrito");
 const selectFPago = document.querySelector(".formapago");
 const finalizarPago = document.querySelector(".finalizar-pago");
 const carritoFijo = document.querySelector(".carrito-fijo");
+
+//Formulario de Login
+const usuarioEmail = document.getElementById('login-email'); //input donde se ingresa el e-mail al momento del login
+const usuarioClave = document.getElementById('login-clave'); //input donde se ingresa la clave al momento del login
+const buttonLogin = document.getElementById('button-login'); //botón 'Ingresar' del formulario Login que tiene
+const errorLogin = document.getElementById('errorLog'); //párrafo que informa que la combinación de E-mail y clave no existen
+
+//Icono de Login en la barra de navegación (izquierda)
+const usuarioLogin = document.querySelector('.user-login'); //es el ícono que está a la izquierda en la barra de navegación
+const nombreUser = document.querySelector('.nombre-user-login'); //etiqueta Nombre a la derecha del ícono del login (arriba izquierda). Se asigna el nombre del usuario cuando se loguea
+const cerrarSesion = document.querySelector('#cierre-sesion'); //cierra la sesión del usuario
+
 export const totalProductos = [...bdProductos];
 let miCarrito = [];
 let totalFinalCarrito = 0;
 let cantCuota = 0;
 let importeCuota = 0;
+let blnModalFinalizarCompra = false;
 
 //------------------------------------------------------------------- EXPRESIONES REGULARES ----------------------------------------------
 const regexEMail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/; //para validar correo electrónico
@@ -61,6 +76,7 @@ class CarritoCompra {
 };
 
 //----------------------------------------------------------------------------- DECLARACION DE FUNCIONES ----------------------------------------------------
+
 //Lee la clave "micarrito" del localStorage. La clave "micarrito" contiene el total de productos agregado al carrito que todavía no se finalizó o concretó la compra.
 export const cargaCarritoLS = () => {
     const micarritoLS = JSON.parse(localStorage.getItem('micarrito')) || []; //se hace la lectura del localstorage para ver si el carrito tiene contenido
@@ -69,12 +85,32 @@ export const cargaCarritoLS = () => {
         miCarrito.push(new CarritoCompra(el.codigo, el.nombre, el.cantidad, el.precio)); //Se agrega el producto en el carrito
     }); -carrito - 1
     actualizaTotalesCarrito(miCarrito); //actualiza total carrito
+
+    //Verifica is existe algún usuario logueado en la clave "Auth" del localStorage
+    if (leeLogin().nombre) {
+        nombreUser.textContent = leeLogin().nombre; //asigna el nombre del usuario a la etiqueta a la derecha del ícono de login
+        cerrarSesion.classList.add('cerrar-sesion'); //muetra párrafo de Cierre de Sesión
+    }
+    else {
+        nombreUser.textContent = "";
+        cerrarSesion.classList.remove('cerrar-sesion');
+    };
 };
 
 //Actualiza la clave "micarrito" en el localStorage
 const seteaCarritoLS = (miCarrito) => {
     localStorage.setItem("micarrito", JSON.stringify(miCarrito));
     actualizaTotalesCarrito(miCarrito); //actualiza total carrito
+};
+
+//Lee la clave "isLogin" del localStorage. Esta clave contiene el valor "true" si el usuario ya está logueado o "false" de lo contrario.
+const leeLogin = () => {
+    return JSON.parse(localStorage.getItem('Auth')) || { isLogin: false, nombre: "" };
+};
+
+//Setea la clave "isLogin" del localStorage. Esta clave contiene el valor "true" si el usuario ya está logueado o "false" de lo contrario.
+const setLogin = (opcion, nombreUsuario) => {
+    localStorage.setItem("Auth", JSON.stringify({ isLogin: opcion, nombre: nombreUsuario }));
 };
 
 //Verifica si el producto existe en el carrito. Si existe retorna o devuelve la cantidad de ese producto agregada al carrito y si no lo encuentra devuelve cero.
@@ -225,10 +261,22 @@ const agregaHtmlCarrito = (miCarrito) => {
         const eliminarProducto = document.querySelector(".modal-productos-eliminar");
         //evento "click" para eliminar un producto del carrito
         eliminarProducto.addEventListener("click", () => {
-            confirmaEliminarProducto(miCarrito, el.codigo, el.nombre);
+            confirmaEliminarProducto(miCarrito, el.codigo, el.nombre); user - login
         });
 
     });
+};
+
+//Verifica si el usuario está logueado antes de proceder a agregar algún producto al carrito o acceder al carrito de compras
+//Si no está logueado, solicita su correo y contraseña de acceso
+const validaIngreso = () => {
+    if (leeLogin().isLogin === false) {
+        usuarioClave.value = '',usuarioEmail.value = ''; //limpia las cajas de email y clave
+        errorLogin.classList.remove('errorLog--show');//limpia el texto de error login
+        modalShowToggleLogin();
+        return false;
+    };
+    return true;
 };
 
 //muestra el contenido del carrito de compras
@@ -360,6 +408,12 @@ const modalShowToggleCarrito = () => {
 const modalShowToggleFinCompra = () => {
     modalFinCompra.classList.toggle('modal-fincompra--show');
 };
+const modalShowToggleLogin = () => {
+    modalLogin.classList.toggle('modal-login--show');
+};
+const modalShowToggleCerrarSesion = () => {
+    cerrarSesion.classList.toggle('cerrar-sesion');
+};
 
 //Agrega código html del formulario modal de la forma de apago
 const agregaHtmlFPago = () => {
@@ -432,7 +486,7 @@ const validaFPago = (opcionSel, value) => {
             desFPago.textContent = `En ${buscar.cuotas} ${cantPago} de $${impCuota}. Total: $${totalPago}. Recargo ${buscar.tasa}%`;
 
         }
-        else if(buscar.detalles !== "") {
+        else if (buscar.detalles !== "") {
             desFPago.classList.add('descripcion-fpago-siv');
             desFPago.classList.remove('descripcion-fpago-nov');
             desFPago.textContent = `Deberá transferir al CBU Nº ${buscar.detalles}`;
@@ -466,7 +520,6 @@ const confirmaCompra = () => {
         imageWidth: 350,
         imageHeight: 200,
         imageAlt: 'Compra A Confirmar',
-        //icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: 'rgb(11, 168, 11)',
         cancelButtonColor: '#d33',
@@ -477,7 +530,6 @@ const confirmaCompra = () => {
             compraExitosa(); //Si acepta la compra se llama a la función compraExitosa
             Swal.fire({
                 position: 'center',
-                //icon: 'success',
                 imageUrl: './img/graciascompra.jpeg',
                 imageWidth: 350,
                 imageHeight: 200,
@@ -496,6 +548,47 @@ const confirmaCompra = () => {
 const compraExitosa = () => {
     miCarrito.splice(0);
     seteaCarritoLS(miCarrito);
+};
+
+//Valida usuario y clave en el login
+const validaUser = async () => {
+    await fetch('https://fakestoreapi.com/users')
+                .then(res=>res.json()) //en res se obtiene el paquete de datos de la api
+                .then((json) => {
+                    //con map obtengo los campos que necesito del array original y los paso al nuevo array. Si quiero cambiar el nombre a algún campo mirar la segunda parte del map. Por ej. el campo password en el array original pasa a llamarse clave en el nuevo array bdUsuarios
+                    const bdUsuariosExt = json.map(({ email, password, name: {firstname} }) => ({ email, clave: password, nombre: firstname}));
+                    const bdUsuariosTotal = bdUsuariosInt.concat(bdUsuariosExt); //se concatenan los array de los usuarios internos más los usuarios de la api               
+                    //busco si existe el email y clave en el nuevo array bdUsuariosExt obtenido de la API
+                    const userExist = bdUsuariosTotal.find(el => ((el.email).toUpperCase() === (usuarioEmail.value).toUpperCase() && el.clave === usuarioClave.value));
+                    (userExist)
+                        ? bienvenidaUser(userExist.nombre, userExist.email)
+                        : errorLogin.classList.add('errorLog--show');
+                }) 
+                .catch(err=>errorLogin.classList.add('errorLog--show')) //si hay algún error y viene por el catch entonces gestiono el error con lo mismo del operador ternario del (userExist)
+};
+const bienvenidaUser = (nombreUsuario) => {
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Hola ' + nombreUsuario + ', ingresaste a tu cuenta!!',
+        showConfirmButton: false,
+        timer: 1500
+    })
+    setTimeout(() => {
+        setLogin(true, nombreUsuario); //graba Auth = { isLogin: true} en el localStorage
+        modalShowToggleLogin(); //oculta el formulario modal del login
+        muestraNombreUsuario(nombreUsuario); //muestra el nombre de usuario a la derecha del ícono de login en la barra de navegación
+        if(blnModalFinalizarCompra) {
+            limpiaHtmlFPago(); //limpia el html de la forma de pago
+            modalShowToggleFinCompra(); //muestra el modal de la finalización de la compra
+            agregaHtmlFPago(); //agrega el html de la forma de pago
+            blnModalFinalizarCompra=false;
+        };
+    }, 1500);
+};
+const muestraNombreUsuario = (nombreUsuario) => {
+    nombreUser.textContent = nombreUsuario;
+    modalShowToggleCerrarSesion();
 };
 //----------------------------------------------------------------------------- CARGA TODOS LOS EVENTOS ----------------------------------------------------
 export const cargarEventos = () => {
@@ -558,12 +651,15 @@ export const cargarEventos = () => {
     //evento "click" sobre finalizar compra
     finalizarCompra.addEventListener("click", (e) => {
         e.preventDefault();
-        modalShowToggleCarrito(); //cierra el modal del carrito
-        setTimeout(() => {
-            limpiaHtmlFPago();
-            modalShowToggleFinCompra(); //muestra el modal de la finalización de la compra
-            agregaHtmlFPago();
-        }, 200);
+        blnModalFinalizarCompra = true;
+        if (validaIngreso()) {
+            modalShowToggleCarrito(); //cierra el modal del carrito
+            setTimeout(() => {
+                limpiaHtmlFPago();
+                modalShowToggleFinCompra(); //muestra el modal de la finalización de la compra
+                agregaHtmlFPago();
+            }, 200);
+        };
 
     });
 
@@ -585,28 +681,70 @@ export const cargarEventos = () => {
         modalShowToggleFinCompra();
     });
 
+    //evento "change" en el dropdown de las opciones de pago en el formulario de finalización de la compra
     selectFPago.addEventListener("change", (e) => {
         e.preventDefault();
         validaFPago(selectFPago.selectedIndex, selectFPago.value);
     });
 
+    //evento "click" en el botón de finalizar compra. Verifica si el usuario está logueado, de lo contrario solicita que se loguee
     finalizarPago.addEventListener("click", (e) => {
         e.preventDefault();
         if (validacionDatosPersonales() && validaFPago(selectFPago.selectedIndex)) {
             confirmaCompra();
         }
     })
+
+    //evento "click" para volver al carrito de compras desde el formulario de finalización de la compra
     volverAlCarrito.addEventListener("click", (e) => {
         e.preventDefault();
         modalShowToggleFinCompra();
         modalShowToggleCarrito();
     });
 
-    //evento "keydown" con key = "Escape" para poder cerrar el modal del carrito con la tecla "ESC"
+    //Botón de Inicio o Login
+    buttonLogin.addEventListener("click", () => {
+        validaUser();
+    });
+
+    //evento "input" que al borrar el input clave borra el mensaje de error de login
+    usuarioEmail.addEventListener("input", (e) => {
+        const { value } = e.target; //desestructuración del objeto "e" para obtener el atributo value
+        !value && errorLogin.classList.remove('errorLog--show');
+    });
+    //evento "input" que al borrar el input clave borra el mensaje de error de login
+    usuarioClave.addEventListener("input", (e) => {
+        const { value } = e.target; //desestructuración del objeto "e" para obtener el atributo value
+        !value && errorLogin.classList.remove('errorLog--show');
+    });
+
+    //evento "Enter" de la caja "clave" del login
+    usuarioClave.addEventListener("keypress", (e) => {
+        if (e.key === 'Enter') {
+            validaUser();
+        };
+    });
+
+    //evento "click" sobre ícono de la barra de navegación "user-login"
+    usuarioLogin.addEventListener("click", (e) => {
+        e.preventDefault();
+        validaIngreso();
+    });
+
+    //evento "click" sobre cerrar sesión del usuario
+    cerrarSesion.addEventListener("click", (e) => {
+        e.preventDefault();
+        setLogin(false, ""); //actualiza la clave "Auth" del localStorage
+        nombreUser.textContent = ""; //borra el nombre del usuario logueado
+        modalShowToggleCerrarSesion(); //oculta el cierre de sesión
+    });
+
+    //evento "keydown" con key = "Escape" para poder cerrar los formularios modales que estén abiertos, presionando la tecla "ESC"
     document.addEventListener("keydown", (e) => {
         if (e.key === "Escape") {
             modal.classList.remove('modal--show');
             modalFinCompra.classList.remove('modal-fincompra--show');
+            modalLogin.classList.remove('modal-login--show');
         }
     });
 };
